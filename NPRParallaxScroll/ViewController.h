0bcb5d186e71d248ce7a1ff4b2a8aaaea280f619
Nico Prananta <nicnocquee@yahoo.com>
2013-04-30 23:24:42 +0900

    Initial Commit

---
+++ NPRParallaxScroll/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NPRParallaxScroll
+//
+//  Created by Nico Prananta on 4/30/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
